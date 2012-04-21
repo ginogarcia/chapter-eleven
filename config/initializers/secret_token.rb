13feaf2e3821ceca397aa79ddfa58f55f9ea330b
Gino Garcia <ginogarcia@netzero.net>
2012-04-20 22:45:11 -0500

    starting chapter 11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapterEleven::Application.config.secret_token = 'd02cbd80b759212bb1f33e05c3a033afff59169fd574388101e38d254770a8737bfe3ad383a4d0b866890c5a5a6943c72f3048ac5e753ee5b06a1d7b28fd6fb9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChapterEleven::Application.config.session_store :cookie_store, :key => '_chapter-eleven_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChapterEleven::Application.config.session_store :active_record_store
